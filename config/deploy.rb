3c1b193e034b7085bf2e517e275d128782d4b527
david1497 <david.junior1214@gmail.com>
2016-06-09 10:38:24 +0300

    ckeditor

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,3 +47,9 @@ namespace :deploy do
   end
 
 end
+
+desc 'copy ckeditor nondigest assets'
+task :copy_nondigest_assets, roles: :app do
+  run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} ckeditor:create_nondigest_assets"
+end
+after 'deploy:assets:precompile', 'copy_nondigest_assets'
