06a813a4ebcbd01a67b9f1863269dda679a984d4
david1497 <david.junior1214@gmail.com>
2016-06-12 01:16:37 +0300

    finish the content editing

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #get '/app/views/sigurance.html.erb'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
